7d765e70c6398796bdcd7f8848ab4879c06dc605
absonner <anna.onnerstad@cern.ch>
2022-07-22 12:54:57 +0300

    What I managed in one hour

--- Template.C
+++ Template.C
@@ -19,9 +19,17 @@ using namespace std;
 //Declaration of histograms and constants
 TH1D *hDeltaPhiHM = new TH1D("hDeltaPhiHM","hDeltaPhiHM",200, 0.0, 2.0*TMath::Pi());
 TH1D *hDeltaPhiLM = new TH1D("hDeltaPhiLM","hDeltaPhiLM",200, 0.0, 2.0*TMath::Pi());
+TH1D *hYPrime[F_trials+1];
+TF1 *fFitarray[F_trials+1];
+
 const int NH=2;
-const double Fit_min=1.0;
-const double Fit_max=3.0;
+const double F_min=1.0;
+const double F_max=3.0;
+const int F_trials = 100;
+
+for( int iF=0; iF<=F_trials; iF++){
+		hYPrime[iF] = new TH1D(Form("hYPrime%03d"),Form("F=%d",Farray[iF]),200,0.0, 2.0*TMath::Pi());
+	}
 //Member functions
 void LoadData(TString inputfile);
 
@@ -39,11 +47,33 @@ void Template(){
 	strformula+=")";
 	cout<<strformula<<endl;
 
-	// Y'=Y_HM-F*Y_LM
-	hDeltaPhiHM->Add(hDeltaPhiLM,-1);// need to get F in there somehow
+	//defining fit function + setting parameters
+	TString ParName[NH+1]={"const","v_2","v_3"};
+	TF1 *fFit = new TF1("fFit", strformula, 0, 2.0*TMath::Pi());
+	fFit->SetParameter(0,1E4);
+	for (Int_t i=1; i<=NH; i++) fFit->SetParameter(i,0.06);
+	for (Int_t i=0; i<=NH+1; i++) fFit->SetParName(i,ParName[i]);
 
-	//Fit Y'
+	//defining the different values for F
+	double F_stepsize = (F_max-F_min)/F_trials;
+	double Farray[F_trials+1]={};
+	double F = F_min;
+	Farray[0]=F;
+	for( int iF=1; iF<=F_trials; iF++){
+		F=+F_stepsize;
+		Farray[iF]=F;
+	}
 
+	for( int iF=0; iF<=F_trials; iF++){
+		//Multiplying Y_LM with F so that Add function can be used later for substraction.
+		hDeltaPhiLM->Scale(Farray[iF]);
+		// Y'=Y_HM-F*Y_LM
+		hYPrime[iF]=hDeltaPhiHM->Add(hDeltaPhiLM,-1);
+		//Fitting Y'
+		hYPrime[iF]->Fit("fFit");
+		fFitarray[iF]=fFit;
+	}
+	
 	//chi^2
 
 	//find best F parameters
